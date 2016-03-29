bafd243f161e7ee1d80cf75e52d89554671b2475
bclous <brian.clouser@gmail.com>
2016-03-29 14:43:21 -0400

    first commit

---
+++ DidYou/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DidYou
+//
+//  Created by Brian Clouser on 3/29/16.
+//  Copyright © 2016 Did You Nooglers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DidYou/Secrets.h
@@ -0,0 +1,13 @@
+//
+//  Secrets.h
+//  DidYou
+//
+//  Created by Brian Clouser on 3/29/16.
+//  Copyright © 2016 Did You Nooglers. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Secrets : NSObject
+
+@end
